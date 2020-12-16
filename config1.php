2dc96617c862144df91d438022bd7d52b693c33a
Milkawop <75690857+Milkawop@users.noreply.github.com>
2020-12-16 17:52:05 +0300

    Update config1.php

--- config1.php
+++ config1.php
@@ -1,14 +1,13 @@
-<? php
-ob_start ();
-session_start ();
+<?<?php
+ob_start();
+session_start();
 
-date_default_timezone_set ( «Европа / Лондон» );
+date_default_timezone_set("Europe/London");
 
-попробуйте {
-	$ con = новый  PDO ( "mysql: db_name = university; host = localhost" , "root" , "" );
-	$ con -> setAttribute ( PDO :: ATTR_ERRMODE , PDO :: ERRMODE_WARNING );
+try{
+	$con = new PDO("mysql:db_name=university;host=localhost","root","");
+	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }
-catch ( PDOException  $ e ) {
-	exit ( "Ошибка подключения" . $ e -> getMessage ());
+catch(PDOException $e){
+	exit("Connection failed" . $e->getMessage());
 }
-?> 
